5198d50afd9f89e18498fc670d0856dc14da5dc3
Philip Chang <sgnoohc@gmail.com>
2017-04-30 01:51:57 -0700

    update

--- python/makeplot.py
+++ python/makeplot.py
@@ -855,8 +855,8 @@ class HistogramPainter:
             totalbkghist.SetFillStyle(3245)
             totalbkghist.SetFillColor(12)
             totalbkghist.Draw('E2 same')
-        print 'totalbkghist'
-        totalbkghist.Print('all')
+        #if totalbkghist:
+        #    totalbkghist.Print('all')
         #print totalbkghist.Integral(2,3)
         #print totalbkghist.Integral(19,25)
         #print totalbkghist.Integral(19,26)
