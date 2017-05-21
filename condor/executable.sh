21953ff62c01937aef93ba1cdcca0b0a614e236b
Philip Chang <sgnoohc@gmail.com>
2017-05-21 11:14:49 -0700

    msg

--- condor/executable.sh
+++ condor/executable.sh
@@ -17,16 +17,17 @@ NEVENTS=$5
 
 if [ -e ${SAMPLEFILENAME}_skimtree.root ]; then
   gfal-copy -p -f -t 4200 file://`pwd`/${SAMPLEFILENAME}_skimtree.root srm://bsrm-3.t2.ucsd.edu:8443/srm/v2/server?SFN=/hadoop/cms/store/user/phchang/ditto_skimmed_babies/${ANALYSIS}_${TAG}/${SAMPLEFILENAME}_skimtree.root --checksum ADLER32
+  rm ${SAMPLEFILENAME}_skimtree.root
 else
   echo "Ditto::CondorExecutuable:: No *_skimtree.root files to copy..."
 fi
 
-rm ${SAMPLEFILENAME}_skimtree.root
+ls -l
 
 OUTPUTFILES=$(ls ${SAMPLEFILENAME//_skimtree}_*.root)
 #if [ -e ${SAMPLEFILENAME//_skimtree}_hist.root ]; then
 if [[ ${OUTPUTFILES} == *"No such"* ]]; then
-  echo "Ditto::CondorExecutuable:: No *_hist.root files to copy..."
+  echo "Ditto::CondorExecutuable:: No other output files to copy..."
 else
   gfal-copy -p -f -t 4200 file://`pwd`/${SAMPLEFILENAME//_skimtree}_*.root srm://bsrm-3.t2.ucsd.edu:8443/srm/v2/server?SFN=/hadoop/cms/store/user/phchang/ditto_output/${ANALYSIS}_${TAG}/${SAMPLEFILENAME//_skimtree}_hist.root --checksum ADLER32
 fi
