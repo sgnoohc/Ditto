773cb632f74cf59f8ad244ceb4261bf3db9ad866
Philip Chang <sgnoohc@gmail.com>
2017-06-01 14:41:23 -0700

    update

--- src/DittoLoopUtil.cxx
+++ src/DittoLoopUtil.cxx
@@ -437,7 +437,7 @@ namespace LoopUtil
     eventlistloaded = true;
   }
 
-  bool failed(std::vector<int> eventid, float cutID, TString message)
+  bool failed(std::vector<int> eventid, int cutID, float cutval, TString message)
   {
     if (!LoopUtil::eventlistloaded) return false;
     std::vector<std::vector<int> >::iterator it;
